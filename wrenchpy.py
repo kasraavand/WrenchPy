7101bc40120b57ba810b9f4b6494fffa7e1697a7
kasramvd <kasraavand@gmail.com>
2016-09-25 19:26:20 +0330

    add Find_largest_path function to data structure class

--- wrenchpy.py
+++ wrenchpy.py
@@ -1,5 +1,6 @@
 from collections import deque
 from itertools import groupby
+from functools import wraps
 from math import sqrt
 
 
@@ -56,6 +57,116 @@ class datastructure:
         self.find_postorder(preorder, prestart + 1, inorder, inostart, i - inostart)
         self.find_postorder(preorder, prestart + i - inostart + 1, inorder, i + 1, length - i + inostart - 1)
         yield preorder[prestart]
+
+    def Find_largest_path(self, total, matrix):
+        """
+        Find most weighted path in a matrix (We only are allowed to move down and right)
+        from top left cell to most bottom right cell which it's subtract from total is the minimum.
+        If there is no path with a positive or zero reminder it returns -1.
+
+        This function uses an optimized algorithm without recursive methods (for sake of memory
+        optimization) and a cache decorator (for sake of time optimization and refuse of extra 
+        calculations for same situations).
+        """
+        N = len(matrix) - 1
+        remained_states = deque()
+        y = x = path_sum = 0
+        all_path = []
+        blind_nodes = set()
+
+        def cache_moves(f):
+            cache = {}
+
+            @wraps(f)
+            def wrapped(*args):
+                try:
+                    result = cache[args]
+                except KeyError:
+                    result = cache[args] = f(*args)
+                return result
+            return wrapped
+
+        def find_next_cells(y, x):
+            if y == x == N:
+                return False
+            next_cells = {(y, min(x + 1, N)), (min(y + 1, N), x)}
+            if (y, x) in next_cells:
+                return next_cells.difference(((y, x),))
+            else:
+                return next_cells
+
+        @cache_moves
+        def move(y, x, path_sum):
+            next_cells = find_next_cells(y, x)
+            if not next_cells:
+                # we are in last cell
+                return False
+            try:
+                next_1, next_2 = next_cells
+            except:
+                new = next_cells.pop()
+                if new in blind_nodes:
+                    return None
+            else:
+                if next_1 in blind_nodes:
+                    new = next_2
+                elif next_2 in blind_nodes:
+                    new = next_1
+                else:
+                    (n_1_y, n_1_x), (n_2_y, n_2_x) = next_1, next_2
+                    if matrix[n_1_y][n_1_x] > matrix[n_2_y][n_2_x]:
+                        new = next_1
+                        remained_states.append((next_2, path_sum))
+                    else:
+                        new = next_2
+                        remained_states.append((next_1, path_sum))
+            finally:
+                return new
+
+        while True:
+            if N == 0:
+                return total
+            next_cell = move(y, x, path_sum)
+            try:
+                new_y, new_x = next_cell
+            except:
+                if next_cell is False:
+                    # we are in last cell
+                    if not remained_states:
+                        if all_path:
+                            result = total - max(all_path)
+                        else:
+                            result = total - (path_sum + matrix[y][x])
+                        if result < 0:
+                            return -1
+                        return result
+
+                    else:
+                        # backtrack
+                        all_path.append(path_sum)
+                        (new_y, new_x), path_sum = remained_states.pop()
+                else:
+                    # we are in blind node with one next
+                    (new_y, new_x), path_sum = remained_states.pop()
+
+            finally:
+                value = matrix[new_y][new_x]
+                if value > total:
+                    blind_nodes.add((new_y, new_x))
+                path_sum += value
+                if new_y == new_x == N and path_sum == total:
+                    return 0
+                if path_sum >= total:
+                    try:
+                        (new_y, new_x), path_sum = remained_states.pop()
+                    except:
+                        if all_path:
+                            return total - max(all_path)
+                        return -1
+                    else:
+                        value = matrix[new_y][new_x]
+                        path_sum += value
+                y, x = new_y, new_x
 
 
 class text:
